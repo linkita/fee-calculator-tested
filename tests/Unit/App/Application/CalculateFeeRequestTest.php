333d7ce0207b895b4e3652b93a110d0708657716
Mavi Jimenez <mavi@linkita.net>
2017-10-15 21:39:41 +0200

    Configure phpunit

---
+++ tests/Unit/App/Application/CalculateFeeRequestTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Linkita\Tests\Unit\App\Application;
+
+use PHPUnit\Framework\TestCase;
+
+class CalculateFeeRequestTest extends TestCase
+{
+
+    public function testAnAssertTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}

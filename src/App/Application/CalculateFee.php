b291706641cfa777cd792492cc7d62005106fd77
Mavi Jimenez <mavi@linkita.net>
2017-10-15 19:17:23 +0200

    Install phpunit :P

--- src/App/Application/CalculateFee.php
+++ src/App/Application/CalculateFee.php
@@ -25,8 +25,7 @@ class CalculateFee
     public function __construct(
         FeeCalculator $feeCalculator,
         TariffRepositoryInterface $tariffRepository
-    )
-    {
+    ) {
         $this->feeCalculator = $feeCalculator;
         $this->tariffRepository = $tariffRepository;
     }
